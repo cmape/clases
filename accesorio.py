2821c2345e5740ffe62f1cdba7cf01e5f681edb2
ccangie94 <99290660+ccangie94@users.noreply.github.com>
2022-03-08 22:24:37 -0500

    Update accesorio.py

--- accesorio.py
+++ accesorio.py
@@ -1,19 +1,22 @@
-class Gorro:
-color =""
-estilo =""
+class Gorro(Object):
+    color =""
+    estilo =""
 
-def __init__(self,color,estilo):
-    self.color=color
-    self.estilo=estilo
+    def __init__(self,color,estilo):
+        self.color=color
+        self.estilo=estilo
+    
+    def Cubrir_frio (self):
+        self.Cubrir_frio
+        print("Tengo un gorro de color {} y el estilo es {}". format(self.color,self.estilo))
 
-def Cubrir_frio (self):
-    self.Cubrir_frio
-
-
-class Reloj
-marca =""
-color =""
-material =""
-
-def __init__(self,marca, color,material):
-    pass
+    Class Reloj(Object):
+        marca =""
+        color =""
+        material =""
+        
+         def __init__(self,marca, color, material):
+            self.marca=marca 
+            self.color=color
+            self.material=material
+            print("Tengo un reloj marca {} color {} y es de material {}". format(self.marca,self.color,self.material))
